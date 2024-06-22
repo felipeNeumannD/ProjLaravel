d44853a5aeea2f747ce48327af943aad3a538e6f
felipeNeumannD <167021953+felipeNeumannD@users.noreply.github.com>
2024-06-22 16:40:57 -0300

    ConsertoInicial

--- example-app/public/index.php
+++ example-app/public/index.php
@@ -1,11 +1,20 @@
 <?php
 
-echo"Oi";
 
-echo "aas";
+use Illuminate\Contracts\Http\Kernel;
+use Illuminate\Http\Request;
 
-echo "TESTE";
+define('LARAVEL_START', microtime(true));
 
-echo "aaaaaaaaaaaa";
+require __DIR__.'/../vendor/autoload.php';
 
-echo "HELLO WORLD";
+$app = require_once __DIR__.'/../bootstrap/app.php';
+
+$kernel = $app->make(Kernel::class);
+
+$response = $kernel->handle(
+    $request = Request::capture()
+)->send();
+
+$kernel->terminate($request, $response);
+
