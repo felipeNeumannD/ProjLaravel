6a5d8dd33781d2f4c2165bf0fc5e80d29551f5c5
felipeNeumannD <167021953+felipeNeumannD@users.noreply.github.com>
2024-06-26 19:30:48 -0300

    UserIni

--- example-app/resources/views/user.blade.php
+++ example-app/resources/views/user.blade.php
@@ -1,16 +1,45 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <meta charset='utf-8'>
-    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Page Title</title>
-    <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
-</head>
-<body>
-    <!-- @if ()
-    
-    @endif -->
-</body>
-</html>
+@extends('Layout.main')
+
+@section('title', 'Cadastro Usuário')
+
+
+@section('content')
+<h1>Configurações do usuário</h1>
+<div>
+    <label for="nome">Nome:</label>
+    <input type="text" id="nome" name="nome">
+</div>
+
+<div>
+    <label for="nascimento">Data de Nascimento:</label>
+    <input type="date" id="nascimento" name="nascimento">
+</div>
+
+<div>
+    <label>Sexo:</label>
+    <input type="radio" id="masculino" name="sexo" value="Masculino">
+    <label for="masculino">Masculino</label>
+
+    <input type="radio" id="feminino" name="sexo" value="Feminino">
+    <label for="feminino">Feminino</label>
+</div>
+
+<div>
+    <label for="email">Email:</label>
+    <input type="email" id="email" name="email">
+</div>
+
+<div>
+    <label>Função:</label>
+    <input type="radio" name="funcao" value="aluno">
+    <input type="radio" name="funcao" value="funcionario">
+</div>
+
+@if ()
+
+@endif
+
+<div>
+    <input type="submit" value="Enviar">
+</div>
+@endsection
--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -2,15 +2,21 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Pessoa;
+Use App\Http\Controllers\PessoaController;
 
 
 //Route::view('/teste', 'teste');
 
-Route::get('/hello', function () {
-    return 'Hello, World!';
-});
+// Route::get('/hello', function () {
+//     return 'Hello, World!';
+// });
 
-$pessoa = Pessoa::all();
+// $pessoa = Pessoa::all();
 
-Route::view('/teste', 'teste', ['pessoa' => $pessoa]);    
+// Route::view('/teste', 'teste', ['pessoa' => $pessoa]);
+
+
+
+Route::get("/user", [PessoaController::class,"index"]);
+Route::get("/events/create", [PessoaController::class,"create"]);
 
