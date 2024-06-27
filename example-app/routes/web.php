389d3e245b6a3166a2993be6124aa8fce294c7aa
Henrique Gehm <henrique.a.gehm@gmail.com>
2024-06-26 22:17:23 -0300

    main blade e testes de controller

--- example-app/routes/web.php
+++ example-app/routes/web.php
@@ -1,22 +1,32 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Models\Pessoa;
+
 Use App\Http\Controllers\PessoaController;
+use App\Http\Controllers\AlunoController;
+use App\Http\Controllers\FuncionarioController;
+use App\Http\Controllers\PlanoController;
+use App\Http\Controllers\EquipamentoController;
 
 
-//Route::view('/teste', 'teste');
-
-// Route::get('/hello', function () {
-//     return 'Hello, World!';
-// });
-
-// $pessoa = Pessoa::all();
-
-// Route::view('/teste', 'teste', ['pessoa' => $pessoa]);
+Route::get('/hello', function () {
+    return 'Hello, World!';
+});
 
 
+// Route::get("/user", [PessoaController::class,"index"]);
+// Route::get("/events/create", [PessoaController::class,"create"]);
 
-Route::get("/user", [PessoaController::class,"index"]);
-Route::get("/events/create", [PessoaController::class,"create"]);
+Route::get('/teste', function () {
+    return view('teste');
+});
 
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('home');
+
+Route::resource('aluno', AlunoController::class);
+Route::resource('funcionario', FuncionarioController::class);
+Route::resource('plano', PlanoController::class);
+Route::resource('equipamento', EquipamentoController::class);
